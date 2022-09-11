4eb4b84170437875f0602b2b1c7ad3011d8debe0
Diogo Santana Freitas <98959411+dxxiogo@users.noreply.github.com>
2022-09-11 16:56:37 -0300

    Implementação de interatividade no cartão

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 function changeCountName(){
-    let countName = document.getElementById("input#form-name");
-    let nameToChange = document.getElementsByClassName("count-information-card");
-    console.log(nameToChange)
-    nameToChange.getElementsByClassName("card_user-name").innerText = countName.value
+    let newCountName = document.querySelector(".form-name");
+    let countName = document.querySelector(".count-information-card");
+    countName.querySelector(".card_user-name").innerHTML = newCountName.value;
+}
+
+function changeCountNumber(){
+    let newCountNumber = document.querySelector(".form-number-card");
+    let countNumber = document.querySelector(".text-card_content");
+    countNumber.querySelector(".card-number").innerHTML = newCountNumber.value;
+}
+
+function changeMouth(){
+    let newMouthData = document.querySelector("input[name= 'mouth']");
+    let dataMouth = document.querySelector(".card-data");
+    dataMouth.querySelector(".text-mouth").innerHTML = newMouthData.value;
+}
+
+function changeYear(){
+    let newYearData = document.querySelector("input[name= 'year']");
+    let dataYear = document.querySelector(".card-data");
+    dataYear.querySelector(".text-year").innerHTML = newYearData.value;
+}
+
+function changeCountCVC(){
+    let newCVC = document.querySelector(".cvc-card");
+    let cardCvc = document.querySelector(".card-back");
+    cardCvc.querySelector(".text-cvc").innerHTML = newCVC.value;
+}
+function authenticateInformation(){
+    form.style.display = 'none'
+    authenticate.style.display = 'flex'
 }
